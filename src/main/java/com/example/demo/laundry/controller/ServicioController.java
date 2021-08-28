e83b5df954dc92c6c75e3e479067794551e133ce
AlexanderPrincipe <manuel.principe@telefonica.com>
2021-08-28 12:06:35 -0500

    getmappinglocalhost

--- src/main/java/com/example/demo/laundry/controller/ServicioController.java
+++ src/main/java/com/example/demo/laundry/controller/ServicioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,7 @@ public class ServicioController {
 		return servicioService.listarPorId(id);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/listarServicio")
 	public List<Servicio> listar() {
 		return servicioService.listar();
